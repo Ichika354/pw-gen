516b820e85851c0f9a185910b4085950d11c73bc
Ichika354 <handa82624@gmail.com>
2022-08-31 14:51:50 +0700

    Update password generator

---
+++ index.js
@@ -0,0 +1,35 @@
+let passwordLength = document.getElementById("passwordLength")
+let password = document.getElementById("password")
+let saveButton = document.getElementById("saveButton")
+
+function generatePassword(len) {
+    const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz"
+    const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const numeric = "1234567890"
+    const simbol = "~`!@#$%^&*()_+{}[]:;|\<,>.?/'"
+
+    const data = lowerAlphabet + upperAlphabet + numeric + simbol
+    let generator = '';
+    for (let index = 0 ; index < len; index++) {
+        generator += data[~~(Math.random() * data.length)]
+    } return generator
+}
+
+
+function getPassword() {
+    const newPasswod = generatePassword(passwordLength.value)
+    password.value = newPasswod
+    setTimeout(() => {
+        alert('Password has been generated')
+
+    },700)
+}
+
+function savePassword() {
+    localStorage.setItem = password.value
+    saveButton.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(`Password saya: ${localStorage.setItem}`))
+    saveButton.setAttribute('download', 'MyPasswordGeneratorLOG.txt')
+    setTimeout (() => {
+        alert('Password has been saved')
+    }, 700)
+}
